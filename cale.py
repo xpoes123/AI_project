999b882a32c9653b41a915076caf945a57b5fb97
xpoes123 <logiccubing@gmail.com>
2018-07-05 23:07:17 -0500

    Thing for cale

---
+++ cale.py
@@ -0,0 +1,16 @@
+import random
+turns = []
+counts = 1
+for i in range(50000000000):
+    test = []
+    while True:
+        test.append(random.randint(1, 4))
+        counts += 1
+        print(counts)
+        if len(set(test)) >= 4:
+            turns.append(len(test))
+            break
+print(turns)
+average = sum(turns)
+average = average/50000000000
+print(average)
